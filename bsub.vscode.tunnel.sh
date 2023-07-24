ae90168403f429b117c873d3dc0b737b452b58cb
Pavel Mazin <iaa.aka@yandex.ru>
2023-07-24 10:37:49 +0100

    kill job on user interrupt

--- bsub.vscode.tunnel.sh
+++ bsub.vscode.tunnel.sh
@@ -5,17 +5,27 @@ mem=${1:-40000}
 cpu=${2:-4}
 queue=${3:-normal}
 gmem=${4:-6000}
+name='vs-code-tunnel'
+logpath=${HOME}/.vs-code-tunnel
 
+mkdir -p $logpath
+
+# set gpu parameters for gpu queues
 gpu=''
 if [[ $queue == *"gpu"* ]]
 then
 	gpu="-gpu \"mode=shared:j_exclusive=no:gmem=${gmem}:num=1\""
 fi
 
-name='vs-code-tunnel'
-logpath=${HOME}/.vs-code-tunnel
+# kill job if interupted
 
-mkdir -p $logpath
+exitfn () {
+    trap SIGINT              # Restore signal handling for SIGINT
+    bkill $(bjobs  -o JOBID -J $name -noheader)
+    exit                     #   then exit script.
+}
+
+trap "exitfn" INT            # Set up SIGINT trap to call function.
 
 jobhost=`bjobs  -o first_host -J ${name} -noheader 2> /dev/null`
 
@@ -34,4 +44,4 @@ then
   bwait -w "started($name)"
 fi
 
-
+trap SIGINT
